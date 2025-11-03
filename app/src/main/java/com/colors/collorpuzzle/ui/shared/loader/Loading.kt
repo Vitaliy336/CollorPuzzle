81b0a509dc912d2e876517abec9f71785a53e731
vitaliy.shevchyk <vitaliy.shevchyk@vakoms.com>
2025-11-03 15:50:56 +0200

    Edit: progressbar theming

--- app/src/main/java/com/colors/collorpuzzle/ui/shared/loader/Loading.kt
+++ app/src/main/java/com/colors/collorpuzzle/ui/shared/loader/Loading.kt
@@ -19,7 +19,6 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -124,7 +123,7 @@ private fun JumpingBox(
                 animation = tween(
                     durationMillis = jumpDurationDown,
                     easing = LinearEasing,
-                    delayMillis = jumpDelay.toInt()
+                    delayMillis = jumpDelay
                 ),
                 repeatMode = RepeatMode.Reverse
             )
